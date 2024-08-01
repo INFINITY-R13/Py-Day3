d4533615f1f00c248f10a222b272d23f078f9038
Raghuraj Singh <hannuseven@gmail.com>
2024-08-01 14:55:22 +0530

    if else practice

---
+++ if-else.py
@@ -0,0 +1,16 @@
+print("Welcome To The Rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+    age = int(input("What is your age? "))
+    if age > 18:
+        print("Please pay $12.")
+    elif age < 15:
+        print("Please pay $5.")    
+    else:
+         print("Please pay $7.")   
+else:
+    print("Sorry, you have to grow taller before you can ride.")
+    
+
