6c6baf9e9daf70fbcd7fdd0a39be8215ea8ddfa7
Raghuraj Singh <130720974+INFINITY-R13@users.noreply.github.com>
2025-09-27 17:47:34 +0530

    revision plus code improvements

--- if-else.py
+++ if-else.py
@@ -1,28 +1,30 @@
-print("Welcome To The Rollercoaster!")
+print("Welcome to the Rollercoaster!")
+
+# Get user height
 height = int(input("What is your height in cm? "))
 bill = 0
 
 if height >= 120:
     print("You can ride the rollercoaster!")
+    
+    # Get age and calculate ticket price
     age = int(input("What is your age? "))
     if age > 18:
-        bill= 12
-        print("Adult tickets are $12.")
+        bill = 100
+        print("Adult tickets are ₹100.")
     elif age < 13:
-        bill = 5
-        print("Child tickets are $5.")      
+        bill = 50
+        print("Child tickets are ₹50.")
     else:
-         bill = 7  
-         print("Teen tickets are $7.")  
-
-    wants_photo = input("Do you want a photo taken? Y or N. ")   
+        bill = 70
+        print("Teen tickets are ₹70.")
+    
+    # Ask for photo option
+    wants_photo = input("Do you want a photo taken? (Y/N): ").strip().upper()
     if wants_photo == "Y":
-        bill += 3
-
-    print(f"Your final bill is ${bill}.")    
-
-
+        bill += 20
+    
+    # Output total bill
+    print(f"Your final bill is ₹{bill}.")
 else:
     print("Sorry, you have to grow taller before you can ride.")
-    
-
