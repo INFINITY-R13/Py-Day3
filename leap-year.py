ac99060394349d7694b883850c4d6f1b4beebf42
Raghuraj Singh <130720974+INFINITY-R13@users.noreply.github.com>
2025-09-27 17:57:53 +0530

    revision plus code improvement

--- leap-year.py
+++ leap-year.py
@@ -1,14 +1,11 @@
-# Leap Year 
+# Leap Year Checker
 
 year = int(input("Which year do you want to check? "))
 
-if year % 4 == 0:
-    if year % 100 == 0:
-        if year % 400 == 0:
-            print("Leap Year")
-        else:
-            print("Not a leap year")    
-    else:
-        print("Leap Year")
+# This single line checks all the leap year rules at once:
+# The year must be divisible by 4, AND not divisible by 100,
+# UNLESS it is also divisible by 400.
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print("Leap Year")
 else:
-    print("Not a leap year")        
+    print("Not a leap year")
